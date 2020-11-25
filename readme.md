344264e8268754c0ee7621da0fe1ba328eaaf47b
Andreas <andreaslegenbauer@Andreass-MBP.attlocal.net>
2020-11-25 16:18:05 -0500

    Initial commit

---
+++ readme.md
@@ -0,0 +1,2 @@
+# spring-security-saml2-service-provider-example
+ This is an example Saml2 service provider application in Spring Boot as part of a tutorial series at https://codetinkering.com/spring-security-saml2-service-provider-example/
---
+++ src/main/java/example/Saml2ExampleServiceProviderApp.java
@@ -0,0 +1,12 @@
+package example;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class Saml2ExampleServiceProviderApp {
+
+    public static void main(String[] args) {
+        SpringApplication.run(Saml2ExampleServiceProviderApp.class, args);
+    }
+}
